b2c3aa287d77b61d0d8ff09febf71a597a55643e
sriaTsuji <tsuji@sria.co.jp>
2021-10-19 07:48:17 +0900

    fix: リファクタリング

--- canvas08_spine/src/main.ts
+++ canvas08_spine/src/main.ts
@@ -10,7 +10,12 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view)
 
-// zunkoのspineデータを読み込んだ後のコールバック関数
+/**
+ * onAssetsLoaded
+ * 東北ずん子のspineデータ（zunko）を読み込んだ後に発火させる関数
+ * @param {object} loader
+ * @param {object} res
+ */
 const onAssetsLoaded = (loader: any, res: any) => {
   // Spine インスタンスを生成
   const zunko = new Spine(res.zunko.spineData)
@@ -19,45 +24,33 @@ const onAssetsLoaded = (loader: any, res: any) => {
   zunko.y = app.screen.height
   // zunkoの大きさをセット
   zunko.scale.set(0.75)
-  // stage にzunkoを追加
+  // stageにzunkoを追加
   app.stage.addChild(zunko)
-  // 初期アニメーションとして idle をセット
+  // 初期アニメーションとして、idleをセット
   zunko.state.setAnimation(0, 'idle', true)
   // 他アニメーションをセット
   zunko.state.addAnimation(1, 'white_flag_hand', false, 0)
   zunko.state.addAnimation(2, 'red_flag_hand', false, 0)
+  // 他アニメーションのアルファ値を0にする
   zunko.state.tracks[1].alpha = 0
   zunko.state.tracks[2].alpha = 0
 
-  // ブラウザ上でのマウスの位置を検知する
+  // 画面上のマウスの位置を検知する処理
   window.addEventListener('mousemove', (event) => {
-    // マウスが、どの象限か取得
-    const resultOrthant = getOrthant({
-      canvasSize: {
-        x: window.innerWidth,
-        y: window.innerHeight
-      },
-      currentPosition: {
-        x: event.clientX,
-        y: event.clientY
-      }
-    })
-    // 画面の高さを100%として、マウスの位置を百分率で取得
-    const heightPercentage = Math.round(
-      getPercentage(window.innerHeight, event.clientY)
-    )
-    // 各アニメーションの alpha 値を更新
-    switch (resultOrthant) {
-      // 第1・4象限（画面右側）
-      case 1:
-      case 4:
-        zunko.state.tracks[1].alpha = (100 - heightPercentage) / 100
+    // マウスが画面の右側か左側かを取得
+    const mousePositon = getMousePosition(window.innerWidth, event.clientX)
+    // マウスの位置の高さ（比率）を取得
+    const heightRatio = getRatio(window.innerHeight, event.clientY)
+    // 各アニメーションのアルファ値を更新
+    switch (mousePositon) {
+      // 画面右側
+      case 'right':
+        zunko.state.tracks[1].alpha = 1 - heightRatio
         zunko.state.tracks[2].alpha = 0
         break
-      // 第2・3象限（画面左側）
-      case 2:
-      case 3:
-        zunko.state.tracks[2].alpha = (100 - heightPercentage) / 100
+      // 画面左側
+      case 'left':
+        zunko.state.tracks[2].alpha = 1 - heightRatio
         zunko.state.tracks[1].alpha = 0
         break
       default:
@@ -72,49 +65,23 @@ const onAssetsLoaded = (loader: any, res: any) => {
 app.loader.add('zunko', '/illust_zunko_flag.json').load(onAssetsLoaded)
 
 /**
- * getCenterPosition
- * 中心点を取得する
- * @param {{ x: number; y: number; }} 要素の座標
- * @return {{ x: number; y: number; }}
+ * getMousePosition
+ * マウスの位置が画面の右側か左側かを取得する
+ * @param {number} allWidth 全体サイズ
+ * @param {number} currentPositionX マウスの現在位置のXの値
+ * @return {"right" | "left"}
  */
-const getCenterPosition = ({ x, y }: { x: number; y: number }) => {
-  return {
-    x: Math.round(x / 2),
-    y: Math.round(y / 2)
-  }
+const getMousePosition = (allWidth: number, currentPositionX: number) => {
+  return currentPositionX >= Math.round(allWidth / 2) ? 'right' : 'left'
 }
 
 /**
- * getOrthant
- * マウスの位置から、どの象限かを取得する
- * @param {{ x: number; y: number; }} canvasSize canvas のサイズ
- * @param {{ x: number; y: number; }} currentPosition mouse の現在位置
- * @return {number} 1〜4
+ * getRatio
+ * 全体（all）を元にして、部分（part）の比率を取得する
+ * @param {number} all 全体
+ * @param {number} part 部分
+ * @return {number} 0〜1。小数点第二位まで含む。
  */
-const getOrthant = ({
-  canvasSize,
-  currentPosition
-}: {
-  canvasSize: { x: number; y: number }
-  currentPosition: { x: number; y: number }
-}) => {
-  // canvas の中心点
-  const centerPosition = getCenterPosition({ x: canvasSize.x, y: canvasSize.y })
-
-  if (currentPosition.x >= centerPosition.x) {
-    return currentPosition.y <= centerPosition.y ? 1 : 4
-  } else {
-    return currentPosition.y <= centerPosition.y ? 2 : 3
-  }
-}
-
-/**
- * getPercentage
- * 全体（all）を元にして、部分（part）の百分率を求める
- * @param {number} all 100%にする値
- * @param {number} part 求める値
- * @return {number}
- */
-const getPercentage = (all: number, part: number) => {
-  return (part * 100) / all
+const getRatio = (all: number, part: number) => {
+  return Math.round((part / all) * 100) / 100
 }
